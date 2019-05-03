a616b436ccfd756c97f3af9a5eabe94075b59fb4
Duoqixiaozei <2504173684@qq.com>
2019-05-03 22:25:07 +0800

    download

---
+++ blog/themes/next/source/js/src/motion.js
@@ -0,0 +1,18 @@
+$(document)
+  .on('sidebar.isShowing', function() {
+    NexT.utils.isDesktop() && $('body').velocity('stop').velocity(
+     {paddingLeft: SIDEBAR_WIDTH},
+      SIDEBAR_DISPLAY_DURATION
+    );
+  })
+  .on('sidebar.isHiding', function() {
+  });
+  ...
+  hideSidebar: function() {
+   NexT.utils.isDesktop() && $('body').velocity('stop').velocity({paddingLeft: 0});
+    this.sidebarEl.find('.motion-element').velocity('stop').css('display', 'none');
+    this.sidebarEl.velocity('stop').velocity({width: 0}, {display: 'none'});
+
+    sidebarToggleLines.init();
+    ...
+}
