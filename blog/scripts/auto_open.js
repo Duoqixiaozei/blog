e704d93baecebb9e19f2dc72b21dab53ce9274fd
Duoqixiaozei <2504173684@qq.com>
2019-05-18 02:59:44 +0800

    download

--- blog/scripts/auto_open.js
+++
@@ -1,5 +0,0 @@
-var spawn = require('child_process').exec;
-// Hexo 3 用户复制这段
-hexo.on('new', function(data){
-  spawn('start  "D:\Sublime Text3\sublime_text.exe" ' + data.path);
-});
--- blog/source/categories/index.md
+++
@@ -1,5 +0,0 @@
----
-title: 分类
-date: 2019-05-03 19:21:01
-type: categories
----
